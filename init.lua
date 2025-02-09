01ca24d53bd6b2a897b33e337561ee7895d8f9ac
ArkSum <markus_dick@gmx.net>
2025-02-09 17:22:59 +0100

    add extras

--- init.lua
+++ init.lua
@@ -78,14 +78,13 @@ require("nvim-treesitter.configs").setup({
 	ignore_install = {},
 })
 
--- Godot Setup make sure to have nmap installed
+-- Godot Setup make sure to have nmap/ncat installed
 local gdscript_config = {
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = {},
 }
 if vim.fn.has("win32") == 1 then
-	-- Windows specific. Requires nmap installed (`winget install nmap`)
 	gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
 end
 require("lspconfig").gdscript.setup(gdscript_config)
