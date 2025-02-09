f65a0ea752043ac077ce826c4411d48532315e42
ArkSum <markus_dick@gmx.net>
2025-02-09 15:25:37 +0100

    add godot

--- init.lua
+++ init.lua
@@ -33,11 +33,57 @@ require("lazy").setup({
 	},
 })
 
--- Startup LSP
-require("lspconfig")["gdscript"].setup({
-	name = "godot",
-	cmd = { "ncat", "127.0.0.1", "6005" },
+-- Setup Treesitter syntax highlighting
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"angular",
+		"css",
+		"csv",
+		"dockerfile",
+		"gdscript",
+		"glsl",
+		"go",
+		"html",
+		"java",
+		"javascript",
+		"json",
+		"lua",
+		"markdown",
+		"php",
+		"python",
+		"ron",
+		"rust",
+		"scss",
+		"sql",
+		"toml",
+		"tsx",
+		"typescript",
+		"yaml",
+	},
+	sync_install = false,
+	auto_install = true,
+	ignore_install = {},
 })
+
+-- Godot Setup
+local gdscript_config = {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {},
+}
+if vim.fn.has("win32") == 1 then
+	-- Windows specific. Requires nmap installed (`winget install nmap`)
+	gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
+end
+require("lspconfig").gdscript.setup(gdscript_config)
 
 -- Set Colorscheme
 vim.cmd([[colorscheme tokyonight-night]])
+
+-- Set Tab as intend width equals 4 Space
+vim.cmd([[
+	set autoindent
+	set noexpandtab
+	set tabstop=4
+	set shiftwidth=4
+]])
