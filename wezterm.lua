c2c4df810d78f58f6a73031adaa9d668086246f1
ArkSum <markus_dick@gmx.net>
2025-03-13 20:42:52 +0100

    update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,7 @@ config.default_cwd = "C:\\Users\\marku"
 
 config.disable_default_key_bindings = true
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = false
 
 config.keys = {
 	{ key = "e", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
