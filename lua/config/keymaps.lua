827252fb53fc3383f27394d412ef1c1d9ce8c494
ArkSum <markus_dick@gmx.net>
2025-03-26 21:43:25 +0100

    no copying on pasting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,2 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- no copying on pasting
+vim.keymap.set("v", "p", "P", { silent = true })
