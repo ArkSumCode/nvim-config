6c157fbd03ebe73babb21c483b536631c818c093
ArkSum <markus_dick@gmx.net>
2025-07-14 22:40:59 +0200

    update snacks

--- lua/plugins/general/snacks.lua
+++ lua/plugins/general/snacks.lua
@@ -3,10 +3,15 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
-		image = { enabled = true, icons = {
-			math = "󰪚 ",
-			chart = "󰄧 ",
-			image = " ",
-		} },
+		image = {
+			enabled = true,
+			icons = {
+				math = "󰪚 ",
+				chart = "󰄧 ",
+				image = " ",
+			},
+		},
+		-- Only show explorer searchbar when typing /
+		picker = { sources = { explorer = { layout = { auto_hide = { "input" } } } } }
 	},
 }
