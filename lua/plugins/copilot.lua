324db0b3e7b875bda7c0826a6f09905046fcdd98
ArkSum <markus_dick@gmx.net>
2025-12-27 18:02:36 +0100

    add melindai

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = {
+			window = {
+				layout = "replace",
+			},
+			headers = {
+				user = "Markus",
+				assistant = "MelindAI",
+				tool = "namen weiß ich noch nicht (TOOL default name) wenn du das hier ließt bitte anpassen",
+			},
+		},
+	},
+}
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -39,8 +39,10 @@ return {
           { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
           { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
           { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+          { action = 'lua require("CopilotChat").open()',              desc = " MelindAI",        icon = " ", key = "m" },
           { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
           { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+
         },
 				footer = function()
 					local stats = require("lazy").stats()
