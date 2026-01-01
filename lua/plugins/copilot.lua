1b1f62490abb9a242bf332bc6075e4c2452c4117
ArkSum <markus_dick@gmx.net>
2026-01-01 18:10:17 +0100

    fix lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,11 +1,3 @@
-local function working_dir()
-	return vim.fn.getcwd()
-end
-
-local function os_separator()
-	return package.config:sub(1, 1)
-end
-
 local function read_file(filepath)
 	local file = io.open(filepath, "r")
 	if not file then
@@ -16,28 +8,23 @@ local function read_file(filepath)
 	return content
 end
 
-local function scan_files_by_ext(dir, ext, files)
-	files = files or {}
-	local scan = vim.loop.fs_scandir(dir)
-	if not scan then
-		return files
-	end
-	while true do
-		local name, type = vim.loop.fs_scandir_next(scan)
-		if not name then
-			break
-		end
+local function scan_files_by_name(name)
+	return vim.fn.glob("**/" .. name, true, true)
+end
 
-		local full_path = dir .. os_separator() .. name
-		if type == "directory" then
-			scan_files_by_ext(full_path, ext, files)
-		elseif type == "file" then
-			if name:sub(-#ext) == ext then
-				table.insert(files, full_path)
-			end
+local function scan_files_by_names(names)
+	local files = {}
+	for _, name in ipairs(names) do
+		local scan = scan_files_by_name(name)
+		for _, file in ipairs(scan) do
+			table.insert(files, file)
 		end
 	end
 	return files
+end
+
+local function scan_files_by_ext(ext)
+	return vim.fn.glob("**/*" .. ext, true, true)
 end
 
 local function make_objects(scheme, mimetype, files)
@@ -53,10 +40,21 @@ local function make_objects(scheme, mimetype, files)
 	return objects
 end
 
-local function project_files(scheme, mimetype, ext)
-	local path = working_dir()
-	local files = scan_files_by_ext(path, ext)
-	print("hello" .. #files)
+local function concat_tables(t1, t2)
+	local result = {}
+	for _, v in ipairs(t1) do
+		table.insert(result, v)
+	end
+	for _, v in ipairs(t2) do
+		table.insert(result, v)
+	end
+	return result
+end
+
+local function project_files(scheme, mimetype, ext, names)
+	local f1 = scan_files_by_ext(ext)
+	local f2 = scan_files_by_names(names)
+	local files = concat_tables(f1, f2)
 	return make_objects(scheme, mimetype, files)
 end
 
@@ -68,7 +66,6 @@ return {
 		},
 		opts = {
 			window = {
-				layout = "replace",
 				title = "MelindAI",
 			},
 			headers = {
@@ -80,35 +77,35 @@ return {
 					description = "Send lua project files",
 					uri = "lua",
 					resolve = function()
-						return project_files("lua", "text/x-lua", ".lua")
+						return project_files("lua", "text/x-lua", ".lua", { "README.md" })
 					end,
 				},
 				rust = {
 					description = "Send rust project files",
 					uri = "rust",
 					resolve = function()
-						return project_files("rust", "text/x-rust", ".rs")
+						return project_files("rust", "text/x-rust", ".rs", { "README.md", "cargo.toml" })
 					end,
 				},
 				java = {
 					description = "Send java project files",
 					uri = "java",
 					resolve = function()
-						return project_files("java", "text/x-java", ".java")
+						return project_files("java", "text/x-java", ".java", { "README.md" })
 					end,
 				},
 				javascript = {
 					description = "Send javascript project files",
 					uri = "js",
 					resolve = function()
-						return project_files("javascript", "text/javascript", ".js")
+						return project_files("javascript", "text/javascript", ".js", { "README.md" })
 					end,
 				},
 				typescript = {
 					description = "Send typescript project files",
 					uri = "ts",
 					resolve = function()
-						return project_files("typescript", "text/typescript", ".ts")
+						return project_files("typescript", "text/typescript", ".ts", { "README.md" })
 					end,
 				},
 			},
